716087adaf5317eeca660d4f3d2ce7616b888155
dgrej <dinogrejo@msn.com>
2021-05-23 19:49:39 -0300

    Create conversorTemperatura.py
    conversor de temperatura F p C

---
+++ CursoUsp/conversorTemperatura.py
@@ -0,0 +1,4 @@
+temperaturaFahrenheit = input("Digite a temperatura em Fahrenheit: ")
+f = float(temperaturaFahrenheit)
+temperaturaCelsius = (f - 32) * 5/9
+print("A temperatura em graus Celsius é: %.2f" %(temperaturaCelsius))
