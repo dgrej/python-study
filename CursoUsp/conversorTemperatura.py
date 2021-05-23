4595122af985a82faf81dffffa409fe76ec7dba7
dgrej <dinogrejo@msn.com>
2021-05-23 19:50:44 -0300

    Update conversorTemperatura.py
    conversor de temp

--- CursoUsp/conversorTemperatura.py
+++ CursoUsp/conversorTemperatura.py
@@ -1,4 +1,4 @@
-temperaturaFahrenheit = input("Digite a temperatura em Fahrenheit: ")
-f = float(temperaturaFahrenheit)
+temperaturaFahrenheit = input("Digite a temperatura em Fahrenheit: ") #input sempre sera str
+f = float(temperaturaFahrenheit) #converte o input para float 
 temperaturaCelsius = (f - 32) * 5/9
-print("A temperatura em graus Celsius é: %.2f" %(temperaturaCelsius))
+print("A temperatura em graus Celsius é: %.2f" %(temperaturaCelsius)) # %2.f diminui o ponto flutuante para 2 numeros
