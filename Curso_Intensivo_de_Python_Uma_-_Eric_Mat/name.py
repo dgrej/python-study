faab70f68138b9ee2f75bbda2698627a64552670
dgrej <dinogrejo@msn.com>
2021-08-14 13:50:40 -0300

    name.py / conserto.py

---
+++ Curso_Intensivo_de_Python_Uma_-_Eric_Mat/name.py
@@ -0,0 +1,4 @@
+name = "ada lovelace"
+print(name.title()) # metodo title deixa as primeira letras maiusculas
+print(name.upper()) # metodo upper deixa todas as letras maiusculas
+print(name.lower()) # metodo lower deixa todas as letras minusculas
--- conserto.py
+++ conserto.py
@@ -9,6 +9,7 @@ restante = (total-dino)
 
 print("A mão de obra pelo conserto é de R$ %s e a fechadura R$ %s" %(servico, fechadura))
 print("o valor total é de: R$",total)
+print("o valor para cada um dos 4 é de R$ %s" %(total/4))
 print("Dino pagará: R$",dino )
 print("o restante do valor é de R$ %s e cada um pagará o valor de R$ %s" %(restante, (restante/3)))
 
