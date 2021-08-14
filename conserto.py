425e29aa3affc0126383ffd9914fce1d6eb284f7
dgrej <dinogrejo@msn.com>
2021-08-14 12:03:29 -0300

    conserto do portao valores

---
+++ conserto.py
@@ -0,0 +1,18 @@
+servico = 400
+fechadura = 105
+total = servico+fechadura
+dino = (total-300)
+restante = (total-dino)
+
+#print("Total score for %s is %s" % (name, score))
+#print("Total score for", name, "is", score)
+
+print("A mão de obra pelo conserto é de R$ %s e a fechadura R$ %s" %(servico, fechadura))
+print("o valor total é de: R$",total)
+print("Dino pagará: R$",dino )
+print("o restante do valor é de R$ %s e cada um pagará o valor de R$ %s" %(restante, (restante/3)))
+
+#otima solucao com %s em print!
+
+
+
