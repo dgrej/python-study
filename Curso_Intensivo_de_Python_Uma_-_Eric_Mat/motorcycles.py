857fad4883d33e1e08ea91c54b93f69c8eeb6236
dgrej <dinogrejo@msn.com>
2021-08-15 15:38:01 -0300

    LISTAS em Python

---
+++ Curso_Intensivo_de_Python_Uma_-_Eric_Mat/motorcycles.py
@@ -0,0 +1,12 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati' # altera a informação [0]
+print(motorcycles) # imprime nova lista alterada [0]
+
+motorcycles.append('dafra') # o metoro .append() acrescenta itens a lista
+print(motorcycles)
+
+motorcycles.insert(2, 'kavasaki') # o metodo .insert isere informação a lista em qualquer posição
+print(motorcycles)
+
