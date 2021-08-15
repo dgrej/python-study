66cbf9a1e6a47dfe771c4d43d8fd91b876226239
dgrej <dinogrejo@msn.com>
2021-08-15 15:12:14 -0300

    listas - bicicletas

---
+++ Curso_Intensivo_de_Python_Uma_-_Eric_Mat/bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())  # listas começam do 0
+print(bicycles[2].title()) #tereceiro iten da lista
+print(bicycles[-1].title()) #-1 devole o ultimo iten -2 o penultimo...
+
+mensagem = "Minha primeira bicicleta foi uma " + bicycles[0].title() + "."
+
+print(mensagem)
