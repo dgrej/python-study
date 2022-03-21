ab586643b873170be23bc37b9587e4e479bb0d03
dgrej <dinogrejo@msn.com>
2022-03-20 22:04:53 -0300

    exercicios semanama 2

---
+++ CursoUsp/Semana2/medias.py
@@ -0,0 +1,11 @@
+#media de 4 notas
+from sys import float_info
+
+
+a=float(input("Digite a primeira nota: "))
+b=float(input("Digite a segunda nota: "))
+c=float(input("Digite a terceira nota: "))
+d=float(input("Digite a quarta nota: "))
+soma=a+b+c+d
+media=soma/4
+print("A media aritimetica é {}".format(media))
---
+++ CursoUsp/Semana2/quadrado.py
@@ -0,0 +1,6 @@
+#calculo de perimetro e area de um quadrado
+ladoQuadrado = float(input("Digite o valor correspondente ao lado de um quadrado: "))
+areaQuadrado = ladoQuadrado*ladoQuadrado
+perimetroQuadrado = ladoQuadrado*4
+
+print("perimetro: {} - area: {}".format(perimetroQuadrado,areaQuadrado))
