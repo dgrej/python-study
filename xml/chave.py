2a3a4839f8b4fc86b656f6558647e419e09eb1fe
Dino Grejo <dinogrejo@msn.com>
2024-09-04 12:06:17 -0300

    new cod for path - chave.txt

--- xml/chave.py
+++ xml/chave.py
@@ -1,27 +1,33 @@
-﻿#SCRIPT IN PYTHON BY DINO GREJO - DINOGREJO@GMAIL.COM GITHUB: DGREJ
+﻿# SCRIPT IN PYTHON BY DINO GREJO - DINOGREJO@GMAIL.COM GITHUB: DGREJ
 
 import os
 import re
 
-# pasta contendo os arquivos XML
+# Pasta contendo os arquivos XML
 caminho_pasta = r'C:\Users\dino.grejo\OneDrive - VIVEO\Documentos\python\xml'
 
-# regex para extrair os números da tag chave
+# Caminho para salvar o arquivo chave.txt
+caminho_arquivo_chave = r'C:\Users\dino.grejo\OneDrive - VIVEO\Documentos\python\chave.txt'
+
+# Regex para extrair os números da tag chave
 regex = r'<chCTe>(\d+)</chCTe>'
 
-# abrindo o arquivo de texto para escrita
-with open('chave.txt', 'w') as arquivo_chave:
-    # ESCREVENDO CHAVE NO CABEÇALHO DO TXT
+# Abrindo o arquivo de texto para escrita
+with open(caminho_arquivo_chave, 'w') as arquivo_chave:
+    # Escrevendo "CHAVE" no cabeçalho do arquivo TXT
     arquivo_chave.write('CHAVE\n')
-    # iterando sobre cada arquivo na pasta
+    
+    # Iterando sobre cada arquivo na pasta
     for nome_arquivo in os.listdir(caminho_pasta):
         if nome_arquivo.endswith('.xml'):
-            # abrindo o arquivo XML e lendo o conteúdo
+            # Abrindo o arquivo XML e lendo o conteúdo
             with open(os.path.join(caminho_pasta, nome_arquivo), 'r') as arquivo_xml:
                 conteudo = arquivo_xml.read()
-                # extraindo os números da tag chave usando regex
+                # Extraindo os números da tag chave usando regex
                 match = re.search(regex, conteudo)
                 if match:
                     numero_chave = match.group(1)
-                    # escrevendo o número da chave no arquivo de texto
-                    arquivo_chave.write(numero_chave + ",1" '\n')
+                    # Escrevendo o número da chave no arquivo de texto
+                    arquivo_chave.write(numero_chave + ",1" + '\n')
+
+print(f"Arquivo chave.txt gerado em: {caminho_arquivo_chave}")
